d67cf0b9d715423b9a4e50e12c07bc8fba947e88
viren <virenrangani7@gmail.com>
2026-03-25 17:51:05 +0530

    feat: add new string

--- lib/core/constant/string/custom_string.dart
+++ lib/core/constant/string/custom_string.dart
@@ -66,6 +66,8 @@ class AppStrings {
   static const String otpRequired="OTP is required";
   static const String otpMinChar="Minimum 4 characters required";
 
+  static const String welcomeMessage="Hi, Viren Rangani";
+
 
 
 }
