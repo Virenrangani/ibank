e205588f5b9c124cd66564d60a3e709db0eb4b6c
viren <virenrangani7@gmail.com>
2026-03-26 13:40:07 +0530

    feat: add new image paths to AppImages class

--- lib/core/image/app_image.dart
+++ lib/core/image/app_image.dart
@@ -19,4 +19,14 @@ class AppImage {
 
   static const card="asset/image/Card.png";
 
+  static const b1="asset/image/1.png";
+  static const b2="asset/image/2.png";
+  static const b3="asset/image/3.png";
+  static const b4="asset/image/4.png";
+  static const b5="asset/image/5.png";
+  static const b6="asset/image/6.png";
+  static const b7="asset/image/7.png";
+  static const b8="asset/image/8.png";
+  static const b9="asset/image/9.png";
+
 }
