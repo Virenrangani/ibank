fa979db1b62883eecd76d51455d196de10d3cfbc
viren <virenrangani7@gmail.com>
2026-03-26 10:01:12 +0530

    refactor : add the count on the bell image

--- lib/feature/home/presentation/widget/notification_icon.dart
+++ lib/feature/home/presentation/widget/notification_icon.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:ibank/core/constant/color/custom_color.dart';
+import 'package:ibank/core/constant/font_size/custom_text_style.dart';
 import 'package:ibank/core/image/app_image.dart';
 
 class NotificationIcon extends StatelessWidget {
@@ -7,25 +8,35 @@ class NotificationIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        SizedBox(
-          height: 30,
-          width: 30,
-          child: Image.asset(AppImage.bell),
-        ),
-        Align(
-          alignment: Alignment.topRight,
-          child: Container(
-            height: 14,
-            width: 14,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: AppColors.error
+    return SizedBox(
+      height:36,
+      width: 36,
+      child: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: SizedBox(
+              height: 30,
+              width: 30,
+              child: Image.asset(AppImage.bell),
             ),
           ),
-        )
-      ],
+          Positioned(
+            top: 0,
+            right: 0,
+            child: Container(
+              height: 22,
+              width: 22,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: AppColors.error
+              ),
+              child: Center(child: Text("1",style: AppTextStyles.buttonMedium,)),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
