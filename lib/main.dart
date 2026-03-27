d6f9b072bb163cc0dff9dbe2e7b12936dfa5c56c
viren <virenrangani7@gmail.com>
2026-03-27 18:25:09 +0530

    feat : add the Google font Poppins

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:ibank/di/injection.dart';
-import 'package:ibank/feature/auth/presentation/page/login_page.dart';
-import 'package:ibank/feature/auth/presentation/page/signup_page.dart';
-import 'package:ibank/feature/home/presentation/page/home_page.dart';
+import 'package:ibank/feature/bottom_nav_bar/bottom_nav_bar.dart';
 
 
 void main() {
@@ -15,13 +15,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.light,
+        statusBarBrightness: Brightness.dark,
       ),
-      home: const HomePage()
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            textTheme: GoogleFonts.poppinsTextTheme(),
+            colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+          ),
+          home: const BottomNavBar()
+      ),
     );
   }
 }
