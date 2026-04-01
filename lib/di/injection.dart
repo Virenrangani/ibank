18407252d0b55c230398fa8d2fcfbddec3ff3688
viren <virenrangani7@gmail.com>
2026-04-01 17:18:03 +0530

    feat: add cart dependency

--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -9,6 +9,11 @@ import 'package:ibank/feature/auth/domain/usecase/login_useCase.dart';
 import 'package:ibank/feature/auth/domain/usecase/signup_useCase.dart';
 import 'package:ibank/feature/auth/presentation/cubit/login_cubit.dart';
 import 'package:ibank/feature/auth/presentation/cubit/signup_cubit.dart';
+import 'package:ibank/feature/product/data/data_source/product_data_source.dart';
+import 'package:ibank/feature/product/data/repository_impl/product_repository_impl.dart';
+import 'package:ibank/feature/product/domain/repository/product_repository.dart';
+import 'package:ibank/feature/product/domain/use_case/product_use_case.dart';
+import 'package:ibank/feature/product/presentation/cubit/product_cubit.dart';
 
 
 class Injection {
@@ -24,5 +29,10 @@ class Injection {
     sl.registerLazySingleton<LoginRepository>(()=>LoginRepositoryImpl(sl()));
     sl.registerLazySingleton(()=>LoginUseCase(sl()));
     sl.registerFactory(()=>LoginCubit(sl()));
+
+    sl.registerLazySingleton<ProductDataSource>(()=>ProductDataSourceImpl());
+    sl.registerLazySingleton<ProductRepository>(()=>ProductRepositoryImpl(sl()));
+    sl.registerLazySingleton(()=>ProductUseCase(sl()));
+    sl.registerFactory(()=>ProductCubit(sl()));
   }
 }
