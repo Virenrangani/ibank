19253853a3094b54b97f98ef39844e9f7b78d353
viren <virenrangani7@gmail.com>
2026-04-01 17:22:59 +0530

    refactor: update Dio configuration and integrate interceptor

--- lib/core/network/dio/api_dio.dart
+++ lib/core/network/dio/api_dio.dart
@@ -1,13 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:ibank/core/network/api_url/api_base_url.dart';
+import 'package:ibank/core/network/interceptor/app_interceptor.dart';
 
 import 'package:dio/dio.dart';
 import 'package:ibank/core/network/api_url/api_base_url.dart';
+import 'package:ibank/core/network/interceptor/app_interceptor.dart';
 
 class ApiDio {
-  static Dio dio=Dio(
+
+  static final Dio dio = Dio(
     BaseOptions(
       baseUrl: ApiBaseUrl.authLogin,
-      connectTimeout: const Duration(seconds: 5),
-      receiveTimeout: const Duration(seconds: 5),
+      connectTimeout: const Duration(seconds: 15),
+      receiveTimeout: const Duration(seconds: 15),
+      contentType: 'application/json',
     ),
-  );
+  )..interceptors.add(AuthInterceptor());
 }
